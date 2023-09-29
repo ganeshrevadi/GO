a1e208477e59e81a34510d99bb927d3be873237b
ganeshrevadi <ganeshrevadi16@gmail.com>
2023-09-29 14:37:25 +0530

    Conversions

---
+++ Conversions/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	fmt.Println("Hello World")
+
+	fmt.Println("Enter the Rating:")
+
+	reader := bufio.NewReader(os.Stdin)
+
+	input, _ := reader.ReadString('\n')
+
+	fmt.Println("Thanks for the rating : ", input)
+
+	numRating, err := strconv.ParseFloat(strings.TrimSpace(input), 64)
+
+	if err != nil {
+		fmt.Println("Error : ", err)
+	} else {
+		fmt.Println("Rating : ", numRating+1)
+	}
+}
