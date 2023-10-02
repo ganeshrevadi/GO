50c8a544f455401c9f9fdffe0229abe3180eb982
ganeshrevadi <ganeshrevadi16@gmail.com>
2023-10-02 11:53:02 +0530

    Working with Files in GO

---
+++ Files/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+)
+
+func main() {
+	fmt.Println("Working with files !")
+	content := "This needs to go in a file "
+
+	file, err := os.Create("./myFile.txt")
+	checkNilErr(err)
+	length, err := io.WriteString(file, content)
+	checkNilErr(err)
+
+	fmt.Println("Length is : ", length)
+	defer file.Close()
+	readFile("./myFile.txt")
+}
+
+func readFile(filename string) {
+	databyte, err := ioutil.ReadFile(filename)
+	checkNilErr(err)
+	fmt.Println("Text in the file is :  \n", string(databyte))
+}
+
+func checkNilErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
