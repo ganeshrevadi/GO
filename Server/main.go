93174252be82ab67902cf34b0c997cab0f25d1a0
ganeshrevadi <ganeshrevadi16@gmail.com>
2023-10-04 23:19:29 +0530

    Server Using GOlang

---
+++ Server/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	fmt.Println("Welcome to Server")
+	GetReq()
+}
+
+func GetReq() {
+	const myurl = "http://localhost:8000"
+
+	res, err := http.Get(myurl)
+	if err != nil {
+		panic(err)
+	}
+
+	defer res.Body.Close()
+
+	fmt.Println("Status Code:", res.StatusCode)
+}
