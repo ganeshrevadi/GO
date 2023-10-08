0b3a401d92d431400eb32f9ba7fefd871c9ef267
ganeshrevadi <ganeshrevadi16@gmail.com>
2023-10-08 23:26:17 +0530

    Module

--- Mod/go.mod
+++ Mod/go.mod
@@ -2,4 +2,4 @@ module github.com/ganeshrevadi/GO/Mod
 
 go 1.21.1
 
-require github.com/gorilla/mux v1.8.0 // indirect
+require github.com/gorilla/mux v1.8.0
--- Mod/main.go
+++ Mod/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
 	fmt.Println("Mod in Golang")
+	gretter()
+	r := mux.NewRouter()
+	r.HandleFunc("/", serverHome).Methods("GET")
+
+	log.Fatal(http.ListenAndServe(":4000", r))
+}
+func gretter() {
+	fmt.Println("Hello Mod User")
+}
+
+func serverHome(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<h1>Welcome to Golang</h1>"))
 }
