0f8cc6ad24fc108212d4f1a11431746fdc672675
ganeshrevadi <ganeshrevadi16@gmail.com>
2023-10-09 22:32:14 +0530

    API

--- APi/main.go
+++ APi/main.go
@@ -2,6 +2,20 @@ package main
 
 import "fmt"
 
+type Course struct {
+	CourseID    string  `json:"course_id"`
+	CourseName  string  `json:"course_name"`
+	CoursePrice int     `json:"course_price"`
+	Author      *Author `json:"author"`
+}
+
+type Author struct {
+	FulName string `json:"full_name"`
+	Website string `json:"website"`
+}
+
+var courses []Course
+
 func main() {
 	fmt.Println("Build API with Golang")
 }
