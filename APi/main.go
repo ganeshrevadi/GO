b4308a99e583d58f2831e3a4f33eb5e09d1ab1ac
ganeshrevadi <ganeshrevadi16@gmail.com>
2023-10-09 23:11:36 +0530

    Controllers and MiddleWare

--- APi/main.go
+++ APi/main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"math/rand"
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/gorilla/mux"
 )
@@ -63,5 +66,32 @@ func getOneCourse(w http.ResponseWriter, r *http.Request) {
 	}
 
 	json.NewEncoder(w).Encode("No course Found !")
+	return
+}
+
+func createOneCourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Create One Course")
+	w.Header().Set("Content-Type", "application/json")
+
+	if r.Body == nil {
+		json.NewEncoder(w).Encode("Please Send some data")
+	}
+
+	//what about - {}
+
+	var course Course
+	_ = json.NewDecoder(r.Body).Decode(&course)
+	if course.IsEmpty() {
+		json.NewEncoder(w).Encode("No data")
+		return
+	}
+
+	//generate unique id
+	// append course into course
+	rand.Seed(time.Now().UnixNano())
+
+	course.CourseID = strconv.Itoa(rand.Intn(100))
+	courses = append(courses, course)
+	json.NewEncoder(w).Encode(course)
 	return
 }
