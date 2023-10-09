6603c715f5a8e841198e109bae8c260fe7ace14c
ganeshrevadi <ganeshrevadi16@gmail.com>
2023-10-09 22:56:41 +0530

    API

--- APi/main.go
+++ APi/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 type Course struct {
@@ -34,4 +37,31 @@ func main() {
 
 func serverHome(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("<h1>Welcome to Golang</h1>"))
+}
+
+func getAllCourse(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Get All Course"))
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(courses)
+}
+
+func getOneCourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Get one Course !")
+	w.Header().Set("Content-type", "application/json")
+
+	//grab id from request
+
+	params := mux.Vars(r)
+
+	//loop through course and find matching id and return response
+
+	for _, course := range courses {
+		if course.CourseID == params["id"] {
+			json.NewEncoder(w).Encode(course)
+			return
+		}
+	}
+
+	json.NewEncoder(w).Encode("No course Found !")
+	return
 }
