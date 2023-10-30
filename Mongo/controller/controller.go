44efafe1ddaf5abc7adc63a8582120fc47bc7725
ganeshrevadi <ganeshrevadi16@gmail.com>
2023-10-30 22:53:45 +0530

    Netflix Backend

--- Mongo/controller/controller.go
+++ Mongo/controller/controller.go
@@ -1,12 +1,35 @@
 package controller
 
-import "go.mongodb.org/mongo-driver/mongo/options"
+import (
+	"context"
+	"fmt"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
 
 const connectString = "mongodb+srv://ganeshrevadi16:Grganesh16@@cluster0.dltvwo2.mongodb.net/"
 const dbName = "netflix"
 const collectionName = "watchlists"
 
+var collection *mongo.Collection
+
 func init() {
 	//Client Option
 	clientOption := options.Client().ApplyURI(connectString)
+
+	//connect mongoDB
+	client, err := mongo.Connect(context.TODO(), clientOption)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
+
+	collection = client.Database(dbName).Collection(collectionName)
+
+	//collection instance
+	fmt.Println("Collection instance created!")
+
 }
