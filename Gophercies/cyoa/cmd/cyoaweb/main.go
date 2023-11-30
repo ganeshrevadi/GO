43e8f4c706396a955e9e8cd88dc7193f05dc81ae
ganeshrevadi <ganeshrevadi16@gmail.com>
2023-11-30 23:10:07 +0530

    http Handler and rendering

--- Gophercies/cyoa/cmd/cyoaweb/main.go
+++ Gophercies/cyoa/cmd/cyoaweb/main.go
@@ -2,13 +2,15 @@ package main
 
 import (
 	"cyoa"
-	"encoding/json"
 	"flag"
 	"fmt"
+	"log"
+	"net/http"
 	"os"
 )
 
 func main() {
+	port := flag.Int("port", 3000, "the port to start the CYOA web application on")
 	file := flag.String("file", "gopher.json", "the JSON file with the CYOA story")
 	flag.Parse()
 	fmt.Printf("Using the story in %s.\n", *file)
@@ -19,12 +21,13 @@ func main() {
 		panic(err)
 	}
 
-	d := json.NewDecoder(f)
-	var story cyoa.Story
-	if err := d.Decode(&story); err != nil {
+	story, err := cyoa.JsonStory(f)
+	if err != nil {
 		panic(err)
 	}
 
-	fmt.Printf("%+v\n", story)
+	h := cyoa.NewHandler(story)
+	fmt.Println("Starting the server on port: ", *port)
 
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), h))
 }
--- Gophercies/cyoa/story.go
+++ Gophercies/cyoa/story.go
@@ -1,6 +1,67 @@
 package cyoa
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"html/template"
+	"io"
+	"net/http"
+)
+
+func init() {
+	tpl = template.Must(template.New("").Parse(defaultHandlerTmpl))
+}
+
+var tpl *template.Template
+
+var defaultHandlerTmpl = `
+	<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Choose your own adventure !! </title>
+	</head>
+	<body>
+			<h1>{{.Title}}</h1> 
+			{{range .Paragraphs}}
+				<p>{{.}}</p>
+			{{end}}
+			<ul>
+				{{range .Options}}
+				<li><a href="/{{.Chapter}}">{{.Text}}</a></li>
+				{{end}}
+			</ul>
+	</body>
+	</html>
+`
+
+func NewHandler(s Story) http.Handler {
+	return handler{s}
+}
+
+type handler struct {
+	s Story
+}
+
+// ServeHTTP implements http.Handler.
+
+func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	err := tpl.Execute(w, h.s["intro"])
+	if err != nil {
+		panic(err)
+	}
+
+}
+
+func JsonStory(r io.Reader) (Story, error) {
+	d := json.NewDecoder(r)
+	var story Story
+	if err := d.Decode(&story); err != nil {
+		return nil, err
+	}
+	return story, nil
+}
 
 type Story map[string]Chapter
 
